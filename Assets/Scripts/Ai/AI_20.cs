cb4c81caed8c48fe98a6045a2c6917e28648994f
Dani-PC <danielsoto@correo.ugr.es>
2020-04-17 21:49:25 +0200

    Error

--- Assets/Scripts/Ai/AI_20.cs
+++ Assets/Scripts/Ai/AI_20.cs
@@ -7,7 +7,9 @@ public class AI_20 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //
+        //
+        //
     }
 
     // Update is called once per frame
