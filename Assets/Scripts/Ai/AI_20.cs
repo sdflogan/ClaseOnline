8ce258e9dce0543eeea6ff532d219222a93f4cef
Dani-PC <danielsoto@correo.ugr.es>
2020-04-17 21:50:13 +0200

    Revert "Error"
    This reverts commit cb4c81caed8c48fe98a6045a2c6917e28648994f.

--- Assets/Scripts/Ai/AI_20.cs
+++ Assets/Scripts/Ai/AI_20.cs
@@ -7,9 +7,7 @@ public class AI_20 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //
-        //
-        //
+        
     }
 
     // Update is called once per frame
