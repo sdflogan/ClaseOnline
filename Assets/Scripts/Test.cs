89edf5eef840cd724db00dc2a07b50f49dae6b42
Dani-PC <danielsoto@correo.ugr.es>
2020-04-17 21:20:10 +0200

    Feature Mostrar Anuncio

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,10 @@ public class Test : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MostrarAnuncio()
+    {
+        //
     }
 }
