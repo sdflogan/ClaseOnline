880ab48c17258998dafd76df89ed7aaa7ffe5b02
Dani-PC <danielsoto@correo.ugr.es>
2020-04-17 21:45:36 +0200

    fin avanzada

--- Assets/Scripts/Ai/AI_20.cs
+++ Assets/Scripts/Ai/AI_20.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aim : MonoBehaviour
+public class AI_20 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // ASDFA SADF
+        
     }
 
     // Update is called once per frame
